68f2fd680faba9a80fc4e9f7a4b20d5fc7eafcff
ryoma <ztgk.3142@gmail.com>
2025-01-10 10:53:22 +0900

    fix_lombokでボイラープレートコードを削減

--- src/main/java/com/example/todo_api/controller/sample/SampleDTO.java
+++ src/main/java/com/example/todo_api/controller/sample/SampleDTO.java
@@ -1,22 +1,15 @@
 package com.example.todo_api.controller.sample;
 
+import lombok.Value;
+
 import java.time.LocalDateTime;
 
+@Value
 public class SampleDTO {
 
-    private String content;
-    private LocalDateTime timestamp;
+    String content;
+    LocalDateTime timestamp;
 
-    public SampleDTO(String content, LocalDateTime timestamp) {
-        this.content = content;
-        this.timestamp = timestamp;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
 }
+
+
