2ff1d10068b5cd72fa1d35e93233ecef2e0983e3
ryoma <ztgk.3142@gmail.com>
2025-01-11 11:52:50 +0900

    add_DBを導入

---
+++ src/main/java/com/example/todo_api/repository/sample/SampleRepository.java
@@ -0,0 +1,13 @@
+package com.example.todo_api.repository.sample;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SampleRepository {
+
+    public SampleRecord select() {
+        return new SampleRecord("Hello,World!");
+
+    }
+
+}
--- src/main/java/com/example/todo_api/service/sample/SampleService.java
+++ src/main/java/com/example/todo_api/service/sample/SampleService.java
@@ -1,10 +1,17 @@
 package com.example.todo_api.service.sample;
 
+import com.example.todo_api.repository.sample.SampleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SampleService {
+
+    private final SampleRepository repository;
+
     public SampleEntity find() {
-        return new SampleEntity("Hello");
+        var record = repository.select();
+        return new SampleEntity(record.getContent());
     }
 }
