811a82a771002b8f218a3a32578300b16b9b8939
ryoma <ztgk.3142@gmail.com>
2025-01-13 16:48:11 +0900

    add_レスポンスヘッダへのlocation出力を実装

--- src/main/java/com/example/todo_api/controller/task/TaskController.java
+++ src/main/java/com/example/todo_api/controller/task/TaskController.java
@@ -13,6 +13,8 @@ import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 @RequiredArgsConstructor
 public class TaskController implements TasksApi {
@@ -40,7 +42,7 @@ public class TaskController implements TasksApi {
         dto.setTitle(entity.getTitle());
 
         return ResponseEntity
-                .status(HttpStatus.CREATED)
+                .created(URI.create("/tasks/"+dto.getId()))
                 .body(dto);
     }
 }
